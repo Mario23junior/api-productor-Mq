dc88b7882469491b47a679a497c9f35f1188f2ed
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-04-17 00:59:49 -0300

    Criando dto para o envio da message

---
+++ src/main/java/com/api/project/model/Message.java
@@ -0,0 +1,19 @@
+package com.api.project.model;
+
+public class Message {
+
+	private String text;
+
+	public Message() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
